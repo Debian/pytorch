2a778c03205ef51c65c948f6640f8cb9e76daf21
Mo Zhou <cdluminate@gmail.com>
2020-12-16 19:33:10 +0800

    update python part of autopkgtest again.
    Gbp-Dch: Ignore

--- debian/tests/gencontrol-pytest.sh
+++ debian/tests/gencontrol-pytest.sh
@@ -3,70 +3,72 @@ set -e
 echo "# Generating Autopkgtest Test Cases for the Python Testing Programs"
 echo "# The tests are collected from test/run_test.py"
 FILES=(
+    'distributed/test_c10d'
+    'distributed/test_c10d_spawn'
+    'distributed/test_data_parallel'
+    'distributed/test_distributed_fork'
+    'distributed/test_distributed_spawn'
+    'distributed/test_nccl'
     'test_autograd'
     'test_bundled_inputs'
     'test_complex'
     'test_cpp_api_parity'
-    'test_cpp_extensions_aot_no_ninja'
     'test_cpp_extensions_aot_ninja'
+    'test_cpp_extensions_aot_no_ninja'
     'test_cpp_extensions_jit'
-    'distributed/test_c10d'
-    'distributed/test_c10d_spawn'
     'test_cuda'
-    'test_jit_cuda_fuser'
-    'test_jit_cuda_fuser_legacy'
-    'test_jit_cuda_fuser_profiling'
     'test_cuda_primary_ctx'
     'test_dataloader'
-    'distributed/test_data_parallel'
-    'distributed/test_distributed_fork'
-    'distributed/test_distributed_spawn'
     'test_distributions'
     'test_expecttest'
     'test_foreach'
     'test_indexing'
     'test_jit'
+    'test_jit_cuda_fuser'
+    'test_jit_cuda_fuser_legacy'
+    'test_jit_cuda_fuser_profiling'
     'test_linalg'
     'test_logging'
     'test_mkldnn'
+    'test_mobile_optimizer'
     'test_multiprocessing'
     'test_multiprocessing_spawn'
-    'distributed/test_nccl'
+    'test_namedtuple_return_api'
     'test_native_functions'
     'test_nn'
     'test_numba_integration'
     'test_ops'
     'test_optim'
-    'test_mobile_optimizer'
-    'test_xnnpack_integration'
-    'test_vulkan'
     'test_quantization'
-    'test_sparse'
-    'test_spectral_ops'
     'test_serialization'
     'test_show_pickle'
+    'test_sparse'
+    'test_spectral_ops'
     'test_tensor_creation_ops'
     'test_torch'
-    'test_type_info'
     'test_type_hints'
+    'test_type_info'
     'test_unary_ufuncs'
     'test_utils'
     'test_vmap'
-    'test_namedtuple_return_api'
+    'test_vulkan'
+    'test_xnnpack_integration'
 )
 
 PERMISSIVE_LIST=(
+	distributed/test_c10d 
+	distributed/test_c10d_spawn 
+    distributed/test_data_parallel
+	distributed/test_distributed_fork 
+	distributed/test_distributed_spawn 
+	distributed/test_nccl 
 	test_autograd 
-	test_c10d 
-	test_c10d_spawn 
 	test_cpp_api_parity 
 	test_cpp_extensions_aot_ninja 
 	test_cpp_extensions_aot_no_ninja 
 	test_cpp_extensions_jit 
 	test_cuda
 	test_dataloader
-	test_distributed_fork 
-	test_distributed_spawn 
 	test_distributions 
 	test_jit
 	test_jit_cuda_fuser
@@ -77,7 +79,6 @@ PERMISSIVE_LIST=(
 	test_mobile_optimizer
 	test_multiprocessing_spawn
 	test_native_functions
-	test_nccl 
 	test_nn 
 	test_ops 
 	test_optim 
